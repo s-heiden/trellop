6b732077a23481a089f70002a7318328091d37cc
Stefan Heiden <stefan.w.heiden@gmail.com>
2018-08-28 20:26:41 +0200

    feat: reads a file provided by a command line arg

--- trellop.py
+++ trellop.py
@@ -1,14 +1,23 @@
+import argparse
 import json
 from pprint import pprint
 
+
 def main():
-    parse_trellop_json()
+    # parse command line args
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("-f", "--file", help="location of the property file")
+    args = arg_parser.parse_args()
+
+    if args.file:
+        settings = parse_trellop_json(args.file)
+        pprint(settings)
 
 
-def parse_trellop_json():
-    with open('trellop.json') as trellop_json:
+def parse_trellop_json(filename):
+    with open(filename) as trellop_json:
         data = json.load(trellop_json)
-        pprint(data)
+        return data
 
 
 main()
