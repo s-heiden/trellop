f5769c6a2123e0b97bfec4edc6107d055befff3e
Stefan Heiden <stefan.w.heiden@gmail.com>
2018-08-30 21:44:27 +0200

    feat: creates simple card based on trellop.json using Trello API

--- trellop.py
+++ trellop.py
@@ -2,6 +2,10 @@ import argparse
 import json
 from pprint import pprint
 
+import requests
+
+import constants
+
 
 def main():
     # parse command line args
@@ -13,11 +17,31 @@ def main():
         settings = parse_trellop_json(args.file)
         pprint(settings)
 
+        post_cards(settings)
+
 
 def parse_trellop_json(filename):
     with open(filename) as trellop_json:
         data = json.load(trellop_json)
         return data
 
+
+def post_cards(settings):
+    url = constants.API_URL + 'cards/?key=' + settings['api_key'] + '&token=' + settings['api_token']
+
+    print(url)
+
+    for card_property in settings['cards_properties']:
+        data = {
+            'name': card_property['name'],
+            'desc': card_property['desc'],
+            'idList': card_property['idList'],
+            'pos': card_property['pos']
+        }
+
+        r = requests.post(url, data)
+
+        print(r.status_code, r.reason)
+        print(r.text[:300] + '...')
 
 main()
